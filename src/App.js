29a897b1a79ffe1f30b3232a5c81c5c1c00a2592
nhuttvNextleSoft <nhuttv@nexlesoft.com>
2024-04-23 14:47:24 +0700

    bai18

--- src/App.js
+++ src/App.js
@@ -8,10 +8,18 @@ class App extends React.Component {
     address : 'Da Nang',
     age : 20
   }
+  handleUpLoad() {
+    alert(`Hello`)
+  }
+  handleOnMouseOver(event){
+    console.log(event)
+  }
   render() {
     return (
       <div>
         <h1 style={{ textAlign: "center" }}>Hello {this.state.name}</h1>
+        <button onClick={this.handleUpLoad}>Upload Image</button>
+        <button onMouseOver={this.handleOnMouseOver}>Hover </button>
         <Header/>
       </div>
     );
