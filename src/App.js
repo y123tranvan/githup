585c18df6b984f73c841578b475ec4c77b28c667
nhuttvNextleSoft <nhuttv@nexlesoft.com>
2024-04-23 14:00:15 +0700

    bai17

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
-function App() {
-  return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn react with Y va Bep
-    //     </a>
-    //   </header>
-    // </div>
-    <>
-    <h1 style={{textAlign:'center'}}>Hello world</h1>
-    <Header/>
-    </>
-  );
+import React from "react";
+class App extends React.Component {
+  state = {
+    name : 'Y  and cooking',
+    address : 'Da Nang',
+    age : 20
+  }
+  render() {
+    return (
+      <div>
+        <h1 style={{ textAlign: "center" }}>Hello {this.state.name}</h1>
+        <Header/>
+      </div>
+    );
+  }
 }
-
 export default App;
