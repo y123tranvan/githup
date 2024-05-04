c9ab288154030236801792b4372b4f5c75bb07f2
tranvany <rencooking09202@gmail.com>
2024-05-04 16:16:50 +0700

    learn-git

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,13 +5,26 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import {NavLink} from 'react-router-dom'
 const Header = () => {
   return (
-    <Navbar style={{zIndex:10 , opacity:0.7}} expand="lg" className="bg-body-tertiary">
-      <Container> 
+    <Navbar
+      style={{ zIndex: 10, opacity: 0.7 }}
+      expand="lg"
+      className="bg-body-tertiary"
+    >
+      <Container>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <NavLink className="nav-link" to="/">Home</NavLink>
-            <NavLink className="nav-link" to="/user">User</NavLink>
-            <NavLink className="nav-link" to="/admin">Admin</NavLink>
+            <NavLink className="nav-link" to="/">
+              Home
+            </NavLink>
+            <NavLink className="nav-link" to="/user">
+              User
+            </NavLink>
+            <NavLink className="nav-link" to="/admin">
+              Admin
+            </NavLink>
+            <NavLink className="nav-link" to="/">
+              Exit
+            </NavLink>
           </Nav>
           <Nav>
             <NavDropdown title="Setting" id="basic-nav-dropdown">
